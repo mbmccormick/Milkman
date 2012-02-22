9701b05797aeb8ea0884843c9f3fe8eb662a2ff8
Matt McCormick <mbmccormick@gmail.com>
2012-02-21 19:21:12 -0500

    Reverting API key changes. Fixing issue where listpicker controls were showing up improperly when list values were too long.

--- Milkman/EditTaskPage.xaml.cs
+++ Milkman/EditTaskPage.xaml.cs
@@ -313,5 +313,15 @@ namespace Milkman
         }
 
         #endregion
+
+        private void ListPicker_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            ListPicker target = (ListPicker)sender;
+
+            if (target.Items.Count > 5)
+            {
+
+            }
+        }
     }
 }
