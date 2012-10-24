a64c67787606393403d3d9965e32cf0729fda8cf
Matt McCormick <mbmccormick@outlook.com>
2012-10-23 20:32:29 -0400

    Finalizing the Add Task page integration.

--- Milkman/MainPage.xaml.cs
+++ Milkman/MainPage.xaml.cs
@@ -286,7 +286,7 @@ namespace Milkman
             }
             else
             {
-                NavigationService.Navigate(new Uri("/AddTaskPage.xaml"));
+                NavigationService.Navigate(new Uri("/AddTaskPage.xaml", UriKind.Relative));
             }
         }
 
