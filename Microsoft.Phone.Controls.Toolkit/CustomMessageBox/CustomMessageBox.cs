01f0ff17423c3069dffde13d3088ec9e6aa82d9c
Matt McCormick <mbmccormick@outlook.com>
2012-11-01 09:42:45 -0400

    Reverting Phone Toolkit changes, prepping for patch.

--- Microsoft.Phone.Controls.Toolkit/CustomMessageBox/CustomMessageBox.cs
+++ Microsoft.Phone.Controls.Toolkit/CustomMessageBox/CustomMessageBox.cs
@@ -139,11 +139,6 @@ namespace Microsoft.Phone.Controls
         private Color _systemTrayColor;
 
         /// <summary>
-        /// The current opacity of the system tray.
-        /// </summary>
-        private double _systemTrayOpacity;
-
-        /// <summary>
         /// Called when the message is being dismissing.
         /// </summary>
         public event EventHandler<DismissingEventArgs> Dismissing;
@@ -505,14 +500,11 @@ namespace Microsoft.Phone.Controls
             _frame = Application.Current.RootVisual as PhoneApplicationFrame;
             _page = _frame.Content as PhoneApplicationPage;
          
-            // Change the color and opacity of the system tray if necessary.
+            // Change the color of the system tray if necessary.
             if (SystemTray.IsVisible)
             {
                 // Cache the original color of the system tray.
                 _systemTrayColor = SystemTray.BackgroundColor;
-
-                // Cache the original opacity of the system tray.
-                _systemTrayOpacity = SystemTray.Opacity;
 
                 // Change the color of the system tray to match the message box.
                 if (Background is SolidColorBrush)
@@ -523,9 +515,6 @@ namespace Microsoft.Phone.Controls
                 {
                     SystemTray.BackgroundColor = (Color)Application.Current.Resources["PhoneChromeColor"];
                 }
-
-                // Change the opacity of the system tray to match the message box.
-                SystemTray.Opacity = 1.0;
             }
 
             // Hide the application bar if necessary.
@@ -670,11 +659,10 @@ namespace Microsoft.Phone.Controls
             if (restoreOriginalValues)
             {
                 // Set the system tray back to its original 
-                // color and opacity if necessary.
+                // color if necessary.
                 if (SystemTray.IsVisible)
                 {
                     SystemTray.BackgroundColor = _systemTrayColor;
-                    SystemTray.Opacity = _systemTrayOpacity;
                 }
 
                 // Bring the application bar if necessary.
