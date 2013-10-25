707f223e43b501b4e0941e3c8cc1625e643b7ca2
Matt McCormick <matt@mbmccormick.com>
2013-10-25 15:42:34 -0700

    Incomplete fix for async image loading.

--- Milkman.Imaging/FlipTileTemplate.xaml.cs
+++ Milkman.Imaging/FlipTileTemplate.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Milkman.Imaging.Common;
+using System;
 using System.IO.IsolatedStorage;
 using System.Windows;
 using System.Windows.Controls;
@@ -7,8 +8,22 @@ using System.Windows.Media.Imaging;
 
 namespace Milkman.Imaging
 {
+    public class RenderCompleteEventArgs
+    {
+        bool _success;
+        string _path;
+
+        public RenderCompleteEventArgs(bool success, string path)
+        {
+            _success = success;
+            _path = path;
+        }
+    }
+
     public partial class FlipTileTemplate : UserControl
     {
+        public event EventHandler<RenderCompleteEventArgs> RenderLiveTileImageComplete;
+
         public FlipTileTemplate()
         {
             InitializeComponent();
@@ -16,26 +31,49 @@ namespace Milkman.Imaging
 
         public void RenderLiveTileImage(string filename, string title, string content)
         {
-            this.LayoutRoot.Background = new SolidColorBrush(IsolatedStorageHelper.GetObject<Color>("AccentColor"));
+            BitmapImage iconImage = new BitmapImage(new Uri("/Assets/FlipCycleTileSmall.jpg", UriKind.Relative));
+            iconImage.CreateOptions = BitmapCreateOptions.None;
 
-            this.txtTitle.Text = title;
-            this.txtContent.Text = content;
-
-            this.Measure(new Size(336, 336));
-            this.Arrange(new Rect(0, 0, 336, 336));
-            this.UpdateLayout();
-            
-            WriteableBitmap image = new WriteableBitmap(336, 336);
-            image.Render(this, null);
-            image.Invalidate();
-
-            using (IsolatedStorageFile output = IsolatedStorageFile.GetUserStoreForApplication())
+            iconImage.ImageOpened += (s, e) =>
             {
-                using (var stream = output.OpenFile(filename, System.IO.FileMode.OpenOrCreate))
+                try
                 {
-                    image.SaveJpeg(stream, 336, 336, 0, 100);
+                    this.LayoutRoot.Background = new SolidColorBrush(IsolatedStorageHelper.GetObject<Color>("AccentColor"));
+
+                    this.txtTitle.Text = title;
+                    this.txtContent.Text = content;
+
+                    this.imgIcon.Source = iconImage;
+
+                    this.Measure(new Size(336, 336));
+                    this.Arrange(new Rect(0, 0, 336, 336));
+                    this.UpdateLayout();
+
+                    WriteableBitmap image = new WriteableBitmap(336, 336);
+                    image.Render(this, null);
+                    image.Invalidate();
+
+                    using (IsolatedStorageFile output = IsolatedStorageFile.GetUserStoreForApplication())
+                    {
+                        using (var stream = output.OpenFile(filename, System.IO.FileMode.OpenOrCreate))
+                        {
+                            image.SaveJpeg(stream, 336, 336, 0, 100);
+                        }
+                    }
+
+                    if (RenderLiveTileImageComplete != null)
+                    {
+                        RenderLiveTileImageComplete(this, new RenderCompleteEventArgs(true, filename));
+                    }
                 }
-            }
+                catch (Exception ex)
+                {
+                    if (RenderLiveTileImageComplete != null)
+                    {
+                        RenderLiveTileImageComplete(this, new RenderCompleteEventArgs(false, null));
+                    }
+                }
+            };            
         }
     }
 }
--- Milkman.Imaging/FlipTileTemplateWide.xaml.cs
+++ Milkman.Imaging/FlipTileTemplateWide.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Milkman.Imaging.Common;
+using System;
 using System.IO.IsolatedStorage;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,8 @@ namespace Milkman.Imaging
 {
     public partial class FlipTileTemplateWide : UserControl
     {
+        public event EventHandler<RenderCompleteEventArgs> RenderLiveTileImageComplete;
+
         public FlipTileTemplateWide()
         {
             InitializeComponent();
@@ -16,26 +19,44 @@ namespace Milkman.Imaging
 
         public void RenderLiveTileImage(string filename, string title, string content)
         {
-            this.LayoutRoot.Background = new SolidColorBrush(IsolatedStorageHelper.GetObject<Color>("AccentColor"));
+            BitmapImage iconImage = new BitmapImage(new Uri("/Assets/FlipCycleTileSmall.jpg", UriKind.Relative));
+            iconImage.CreateOptions = BitmapCreateOptions.None;
 
-            this.txtTitle.Text = title;
-            this.txtContent.Text = content;
-
-            this.Measure(new Size(691, 336));
-            this.Arrange(new Rect(0, 0, 691, 336));
-            this.UpdateLayout();
-            
-            WriteableBitmap image = new WriteableBitmap(691, 336);
-            image.Render(this, null);
-            image.Invalidate();
-
-            using (IsolatedStorageFile output = IsolatedStorageFile.GetUserStoreForApplication())
+            iconImage.ImageOpened += (s, e) =>
             {
-                using (var stream = output.OpenFile(filename, System.IO.FileMode.OpenOrCreate))
+                try
                 {
-                    image.SaveJpeg(stream, 691, 336, 0, 100);
+                    this.LayoutRoot.Background = new SolidColorBrush(IsolatedStorageHelper.GetObject<Color>("AccentColor"));
+
+                    this.txtTitle.Text = title;
+                    this.txtContent.Text = content;
+
+                    this.imgIcon.Source = iconImage;
+
+                    this.Measure(new Size(691, 336));
+                    this.Arrange(new Rect(0, 0, 691, 336));
+                    this.UpdateLayout();
+
+                    WriteableBitmap image = new WriteableBitmap(691, 336);
+                    image.Render(this, null);
+                    image.Invalidate();
+
+                    using (IsolatedStorageFile output = IsolatedStorageFile.GetUserStoreForApplication())
+                    {
+                        using (var stream = output.OpenFile(filename, System.IO.FileMode.OpenOrCreate))
+                        {
+                            image.SaveJpeg(stream, 691, 336, 0, 100);
+                        }
+                    }
                 }
-            }
+                catch (Exception ex)
+                {
+                    if (RenderLiveTileImageComplete != null)
+                    {
+                        RenderLiveTileImageComplete(this, new RenderCompleteEventArgs(false, null));
+                    }
+                }
+            };
         }
     }
 }
