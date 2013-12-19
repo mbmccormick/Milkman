c237c3d867506f36c3520375f0989a0d4644653b
Matt McCormick <matt@mbmccormick.com>
2013-12-19 09:46:53 -0800

    Removing unnecessary CompensateForRender.

--- Milkman.Imaging/FlipTileTemplateWide.xaml.cs
+++ Milkman.Imaging/FlipTileTemplateWide.xaml.cs
@@ -27,8 +27,6 @@ namespace Milkman.Imaging
             image.Render(this, null);
             image.Invalidate();
 
-            CompensateForRender(image.Pixels);
-
             using (IsolatedStorageFile output = IsolatedStorageFile.GetUserStoreForApplication())
             {
                 using (var stream = output.OpenFile(filename, System.IO.FileMode.OpenOrCreate))
@@ -37,31 +35,6 @@ namespace Milkman.Imaging
                 }
 
                 output.Dispose();
-            }
-        }
-
-        private void CompensateForRender(int[] bitmapPixels)
-        {
-            if (bitmapPixels.Length == 0) return;
-
-            for (int i = 0; i < bitmapPixels.Length; i++)
-            {
-                uint pixel = unchecked((uint)bitmapPixels[i]);
-
-                double a = (pixel >> 24) & 255;
-                if ((a == 255) || (a == 0)) continue;
-
-                double r = (pixel >> 16) & 255;
-                double g = (pixel >> 8) & 255;
-                double b = (pixel) & 255;
-
-                double factor = 255 / a;
-                uint newR = (uint)Math.Round(r * factor);
-                uint newG = (uint)Math.Round(g * factor);
-                uint newB = (uint)Math.Round(b * factor);
-
-                // compose
-                bitmapPixels[i] = unchecked((int)((pixel & 0xFF000000) | (newR << 16) | (newG << 8) | newB));
             }
         }
     }
