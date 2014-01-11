128b0692bfc35542f00dd9cfdfaa06d9b88f72f6
Matt McCormick <mbmccormick@outlook.com>
2014-01-11 09:31:39 -0800

    Fixing issue where dates disappeared on multiselectlist.

--- Milkman/Controls/TaskItemControl.xaml.cs
+++ Milkman/Controls/TaskItemControl.xaml.cs
@@ -13,25 +13,9 @@ namespace Milkman.Controls
             this.Loaded += TaskItemControl_Loaded;
         }
 
-        private bool DarkThemeUsed()
-        {
-            return Visibility.Visible == (Visibility)Application.Current.Resources["PhoneDarkThemeVisibility"];
-        }
-
         private void TaskItemControl_Loaded(object sender, RoutedEventArgs e)
         {
             Task target = (Task)this.DataContext;
-
-            if (this.DarkThemeUsed() == true)
-            {
-                this.stkDark.Visibility = System.Windows.Visibility.Visible;
-                this.stkLight.Visibility = System.Windows.Visibility.Collapsed;
-            }
-            else
-            {
-                this.stkLight.Visibility = System.Windows.Visibility.Visible;
-                this.stkDark.Visibility = System.Windows.Visibility.Collapsed;
-            }
         }
     }
 }
---
+++ Milkman/Controls/TaskItemInfoControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using IronCow;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Milkman.Controls
+{
+    public partial class TaskItemInfoControl : UserControl
+    {
+        public TaskItemInfoControl()
+        {
+            InitializeComponent();
+
+            this.Loaded += TaskItemInfoControl_Loaded;
+        }
+
+        private bool DarkThemeUsed()
+        {
+            return Visibility.Visible == (Visibility)Application.Current.Resources["PhoneDarkThemeVisibility"];
+        }
+
+        private void TaskItemInfoControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task target = (Task)this.DataContext;
+
+            if (this.DarkThemeUsed() == true)
+            {
+                this.stkDark.Visibility = System.Windows.Visibility.Visible;
+                this.stkLight.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                this.stkLight.Visibility = System.Windows.Visibility.Visible;
+                this.stkDark.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
+    }
+}
