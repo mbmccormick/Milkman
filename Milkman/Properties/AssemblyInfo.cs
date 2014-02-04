37f0520cfb9e0f4d88762189aa311476d6f3adcc
Matt McCormick <matt@mbmccormick.com>
2014-02-04 15:12:13 -0800

    Revving version numbers.

--- Milkman/Properties/AssemblyInfo.cs
+++ Milkman/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.15")]
-[assembly: AssemblyFileVersion("3.3.15")]
+[assembly: AssemblyVersion("3.3.16")]
+[assembly: AssemblyFileVersion("3.3.16")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
